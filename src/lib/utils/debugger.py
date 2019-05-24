11bbf53799267c2f7be5ca880ce13ed26480634b
Arun Kumar <arun.anthony.kumar@gmail.com>
2019-05-24 01:06:34 +0100

    Img plot

--- src/lib/utils/debugger.py
+++ src/lib/utils/debugger.py
@@ -217,7 +217,6 @@ class Debugger(object):
     if not self.ipynb:
       for i, v in self.imgs.items():
         self.plt.imshow(v)
-        cv2.imwrite('{}{}.png'.format(prefix, i),v)
       if cv2.waitKey(0 if pause else 1) == 27:
         import sys
         sys.exit(0)
